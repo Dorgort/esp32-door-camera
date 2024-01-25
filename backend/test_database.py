a83616c029f4c193d54f0ccaf4937eb996e64859
Dorgort <62032134+Dorgort@users.noreply.github.com>
2024-01-25 15:00:33 +0100

    Update test_database.py

--- backend/test_database.py
+++ backend/test_database.py
@@ -4,25 +4,33 @@ from psycopg2 import sql
 db_params = {
     'host': 'localhost',
     'port': 5432,
-    'database': 'camera',
-    'user': 'camera',
-    'password': 'camera'
+    'database': 'camera_database',
+    'user': 'user',
+    'password': 'aetjauhwtwgbjreherh'
 }
 
-conn = psycopg2.connect(**db_params)
+message = "hello world"
+sql = """INSERT INTO test (message) VALUES(%s) RETURNING id;"""
 
-# Open a cursor to perform database operations
-cur = conn.cursor()
-# Execute a command: create datacamp_courses table
-cur.execute("""select * from information_schema.tables
-            """)
 
-print (cur.fetchall())
-# Make the changes to the database persistent
-conn.commit()
-# Close cursor and communication with the database
-cur.close()
-conn.close()
+try:
+    conn = psycopg2.connect(**db_params)
+
+    # Open a cursor to perform database operations
+    cur = conn.cursor()
+
+    cur.execute(sql, (message,))
+    print(cur.fetchone()[0])
+
+    # Make the changes to the database persistent
+    conn.commit()
+    cur.close()
+
+except (Exception, psycopg2.DatabaseError) as error:
+    print(error)
+finally:
+    if conn is not None:
+        conn.close()
 
 
 
