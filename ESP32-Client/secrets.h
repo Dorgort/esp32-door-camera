44f979d725bfead1efe7a2850d8f121d703e9542
Dorgort <62032134+Dorgort@users.noreply.github.com>
2024-03-04 16:40:54 +0100

    Add secrets files

---
+++ ESP32-Client/secrets.h
@@ -0,0 +1,15 @@
+// ===========================
+// Enter your WiFi credentials
+// ===========================
+const char* ssid = "";
+const char* password = "";
+
+// ===========================
+// Enter your MQTT server stuff
+// ===========================
+const char* mqtt_server = "";
+const unsigned int mqtt_port = ;
+#define MQTT_USER               ""
+#define MQTT_PASSWORD           ""
+#define MQTT_PUBLISH_TOPIC    ""
+#define MQTT_SUBSCRIBE_TOPIC    ""
---
+++ frontend/js/config.js
@@ -0,0 +1,8 @@
+const config = {
+    MQTT_USER:"",
+    MQTT_PASSWORD:"",
+    MQTT_IP:"",
+    MQTT_PORT:,
+    MQTT_TOPIC:"",
+    MQTT_DOOR_TOPIC:""
+}
